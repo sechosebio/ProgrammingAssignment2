36493974cbc7b1b19a444cfa97433986bd7b0ef3
Jose Sebio <jalopezsebio@gmail.com>
2015-11-22 23:27:27 +0100

    Added functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## make matrix assign to variable x, and initialize m to NULL
+
 
 makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL
+    set <- function(y) {           
+        x <<- y                     ## reassign "new" matrix to x 
+        m <<- NULL                  ## reinitialize m to NULL
+    }
+    get <- function() x
+    setInvmatrix <- function(InvMatrix) m <<- InvMatrix
+    getInvmatrix <- function() m
+    list(set = set, get = get,
+         setInvmatrix = setInvmatrix,
+         getInvmatrix = getInvmatrix)
 
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getInvmatrix()              
+    if(!is.null(m)) {           ## if user had calculated the same matrix before
+        message("get cached data")  
+        return(m)               ## return old result()
+    }
+    data <- x$get()             ##get the uncalculated matrix
+    m <- solve(data, ...)       ## calculate the inverse matrix
+    x$setInvmatrix(m)           ## reassign inverse matrix 
+    m                           
+
 }
